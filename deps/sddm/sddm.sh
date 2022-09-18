cbf90041b1e25dda05ccd716648ef3988ef3b875
Totti <tarekdshli@gmail.com>
2022-09-18 23:02:55 +0300

    Changed SDDM theme name

--- deps/sddm/sddm.sh
+++ deps/sddm/sddm.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-theme="arch"
-
-# TODO add a feature that check the window manager and disables it 
-# grep 'ExecStart=' /etc/systemd/system/display-manager.service | cut -f2 -d  "="
+theme="Arch"
 
 changeDM(){
 current="$(grep 'ExecStart=' /etc/systemd/system/display-manager.service | cut -f2 -d  "=")"
@@ -28,10 +25,10 @@ installSDDM(){
 if command -v sddm &> /dev/null; then
 	if [ -d "/usr/share/sddm/themes/$theme" ]; then  
 		if [ -f "/etc/sddm.conf.d/theme.conf" ];then
-			echo "theme conf file found moving files" 
+			echo "theme conf file found, moving files" 
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
 		else
-			echo "theme conf file not found creating and moving files" 
+			echo "theme conf file not found, creating and moving files" 
 			sudo mkdir /etc/sddm.conf.d/ &&
 			sudo cp $DIR/theme.conf /etc/sddm.conf.d/ &&
 			sudo sed -i -e "s/Current=.*/Current=$theme/g" /etc/sddm.conf.d/theme.conf
