9a6462745bd3183172d487a36ffcfc3dd3743188
Totti <tarekdshli@gmail.com>
2022-09-22 05:48:21 +0300

    Update bluetoothctl.sh

--- cfg/polybar/scripts/bluetoothctl.sh
+++ cfg/polybar/scripts/bluetoothctl.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-if bluetoothctl show | grep -q "Powered: no"; then
-	echo " Bluetooth Off" 
-elif bluetoothctl show | grep -q "Powered: yes"; then
-	if $(bluetoothctl devices Connected) &> /dev/null; then
-		echo " Not Connected"
-	else 
-		echo  $(bluetoothctl devices Connected | head -n1 | cut -d " " -f3,4,5)
-	fi
+btstatus="$(systemctl  status bluetooth.service | grep -i "Active:" | cut -d " " -f7)"
+
+if [[ $btstatus == "active" ]]; then
+	if bluetoothctl show | grep -q "Powered: no"; then
+		echo " Bluetooth Off" 
+	elif bluetoothctl show | grep -q "Powered: yes"; then
+		if $(bluetoothctl devices Connected) &> /dev/null; then
+			echo " Not Connected"
+		else 
+			echo  $(bluetoothctl devices Connected | head -n1 | cut -d " " -f3,4,5)
+		fi
+	fi	
+else 
+	echo " Bluetooth Unavailable" 
 fi
