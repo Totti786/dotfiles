ef83242152d6faf53a446b12bb80b8736e245c53
Totti <tarekdshli@gmail.com>
2022-09-25 08:31:31 +0300

    Update zsh.sh

--- deps/zsh/zsh.sh
+++ deps/zsh/zsh.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 zsh(){
-	if pacman -Q zsh &> /dev/null; then
+	if command -v zsh &> /dev/null; then
 		echo "zsh is installed"
 		if [ ! -d $HOME/.oh-my-zsh ]; then
 			git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
