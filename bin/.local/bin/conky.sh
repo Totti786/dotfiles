39997aef91d7e907f6fcfde777c9387b776f4cb4
Totti <tarekdshli@gmail.com>
2022-10-27 01:47:48 +0300

    added conky support to bspwm

--- bin/.local/bin/conky.sh
+++ bin/.local/bin/conky.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-conkyConfig=$HOME/.config/conky/conky-Openbox.conf
+conkyConfig=$HOME/.config/conky/conky-$(wmctrl -m |sed -n 1p | sed -e 's/Name: //g').conf
 
 if [ -z "$(pgrep conky)" ]; then 
 	conky -c $conkyConfig &> /dev/null
