0dde399ce847b18c0a7bf5e5c489d391816f469b
Totti <tarekdshli@gmail.com>
2022-11-01 09:30:36 +0300

    Update launch.sh

--- cfg/polybar/launch.sh
+++ cfg/polybar/launch.sh
@@ -13,27 +13,26 @@ current_desktop=$(wmctrl -m |sed -n 1p | sed -e 's/Name: //g')
 
 # Fix backlight and network modules
 fix_modules() {
-	
 	# check the graphics card and adjust the backlight module accordingly
 	if [[ -z "$CARD" ]]; then
-		sed -i -e 's/sep backlight/bna/g' "$DIR"/config.ini
+		sed -i -e 's/sep backlight/bna/g' 								   "$DIR"/config.ini
 	elif [[ "$CARD" != *"intel_"* ]]; then
-		sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
+		sed -i -e 's/backlight/brightness/g' 							   "$DIR"/config.ini
 	else 
-		sed -i -e 's/bna/sep backlight/g' "$DIR"/config.ini
+		sed -i -e 's/bna/sep backlight/g' 								   "$DIR"/config.ini
 	fi
 			
 	# check if bspwm is the curren wm and changes the workspaces module 
 	if [[ $current_desktop == "bspwm" ]]; then
-		sed -i -e 's/modules-center = workspaces/modules-center = bspwm/g' "$DIR"/config.ini 
-		sed -i -e 's/override-redirect = .*/override-redirect = true/g' "$DIR"/config.ini 	
-		sed -i -e 's/titlex\b/title/g' "$DIR"/config.ini
-		sed -i -e 's/wm-restack = .*/wm-restack = bspwm/g' "$DIR"/config.ini
+		sed -i -e 's/modules-center = workspaces/modules-center = bspwm/g'  "$DIR"/config.ini 
+		sed -i -e 's/override-redirect = .*/override-redirect = true/g' 	"$DIR"/config.ini 	
+		sed -i -e 's/titlex\b/title/g' 										"$DIR"/config.ini
+		sed -i -e 's/wm-restack = .*/wm-restack = bspwm/g' 					"$DIR"/config.ini
 	else 
-		sed -i -e 's/modules-center = bspwm/modules-center = workspaces/g' "$DIR"/config.ini
-		sed -i -e 's/override-redirect = .*/override-redirect = false/g' "$DIR"/config.ini
-		sed -i -e 's/title\b/titlex/g' "$DIR"/config.ini
-		sed -i -e 's/wm-restack = .*/wm-restack = /g' "$DIR"/config.ini
+		sed -i -e 's/modules-center = bspwm/modules-center = workspaces/g' 	"$DIR"/config.ini
+		sed -i -e 's/override-redirect = .*/override-redirect = false/g'    "$DIR"/config.ini
+		sed -i -e 's/title\b/titlex/g' 										"$DIR"/config.ini
+		sed -i -e 's/wm-restack = .*/wm-restack = /g' 						"$DIR"/config.ini
 		openbox --reconfigure	
 	fi
 }
@@ -53,7 +52,6 @@ set_values() {
 		sed -i -e "s/network_interface = .*/network_interface = $INTERFACE/g" 	${SFILE}
 	fi	
 }
-
 
 # Launch the bar
 launch_bar() {
