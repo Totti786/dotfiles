1be51cdda510b8b0f5a6aa3b56c3c9ca89c8443f
Totti <tarekdshli@gmail.com>
2022-11-01 09:30:09 +0300

    Updated conky to support 1368x768 resolution

--- bin/.local/bin/conky.sh
+++ bin/.local/bin/conky.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-conkyConfig=$HOME/.config/conky/conky-$(wmctrl -m |sed -n 1p | sed -e 's/Name: //g').conf
+res="$(xrandr -q | grep "current" | cut -d "," -f2 | cut -d " " -f5)"
+wm="$(wmctrl -m |sed -n 1p | sed -e 's/Name: //g')"
+conkyConfig=$HOME/.config/conky/conky-"$wm"-"$res".conf
 
 if [ -z "$(pgrep conky)" ]; then 
 	conky -c $conkyConfig &> /dev/null
