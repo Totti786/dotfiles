a5ad4487b2ffb64f52c0aa12235748fb598555a1
Totti <tarekdshli@gmail.com>
2022-11-19 15:53:57 +0300

    small fixes

--- bin/.scripts/dots-update.sh
+++ bin/.scripts/dots-update.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
 dots="$HOME/dotfiles"
-rofi_command="rofi -theme $dots/cfg/rofi/themes/window.rasi"
+rofi_command="rofi -theme ~/.config/rofi/themes/window.rasi"
 
 # Variable passed to rofi
-options="Update\nExit"
+options="Update System\nUpdate Configs\nExit"
 
 chosen="$(echo -e "$options" | $rofi_command -p 'Update Menu' -dmenu -selected-row 0)"
 case $chosen in
-    Update)
+    Update\ System)
+		furminal --float -e yay
+        ;;
+    Update\ Configs)
 		cd $dots
 		alacritty -e sh $HOME/dotfiles/install.sh --update
         ;;
