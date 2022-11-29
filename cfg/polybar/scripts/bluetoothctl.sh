f55f4c76c4a8f5974422a00ed3bd952a53203f76
Totti <tarekdshli@gmail.com>
2022-11-29 03:07:59 +0300

    rewrote the bluetoothctl script

--- cfg/polybar/scripts/bluetoothctl.sh
+++ cfg/polybar/scripts/bluetoothctl.sh
@@ -1,17 +1,38 @@
 #!/bin/sh
 
-btstatus="$(systemctl  status bluetooth.service | grep -i "Active:" | cut -d " " -f7)"
+get(){
+	btstatus="$(systemctl  status bluetooth.service | grep -i "Active:" | cut -d " " -f7)"
+	
+	if [[ $btstatus == "active" ]]; then
+		if bluetoothctl show | grep -q "Powered: no"; then
+			echo " Bluetooth Off" 
+		elif bluetoothctl show | grep -q "Powered: yes"; then
+			if $(bluetoothctl devices Connected) &> /dev/null; then
+				echo " Not Connected"
+			else 
+				echo  $(bluetoothctl devices Connected | head -n1 | cut -d " " -f3,4,5)
+			fi
+		fi	
+	else 
+		echo " Bluetooth Unavailable" 
+	fi
+}
 
-if [[ $btstatus == "active" ]]; then
-	if bluetoothctl show | grep -q "Powered: no"; then
-		echo " Bluetooth Off" 
-	elif bluetoothctl show | grep -q "Powered: yes"; then
-		if $(bluetoothctl devices Connected) &> /dev/null; then
-			echo " Not Connected"
-		else 
-			echo  $(bluetoothctl devices Connected | head -n1 | cut -d " " -f3,4,5)
+connect(){
+	device=$(bluetoothctl devices Paired | head -n1 | cut -d " " -f2)
+	name=$(bluetoothctl devices Paired | head -n1 | cut -d " " -f3,4,5)
+
+	if bluetoothctl show | grep -q "Powered: yes"; then
+		if [[ $(bluetoothctl info) = "Missing device address argument" ]]; then
+			bluetoothctl connect $device && notify-send "$name" "Connected" 
+		else
+			bluetoothctl disconnect && notify-send "$name" "Disconnected"
 		fi
-	fi	
-else 
-	echo " Bluetooth Unavailable" 
-fi
+	else 
+		bluetoothctl power on
+		main
+	fi
+}
+
+
+"$@"
