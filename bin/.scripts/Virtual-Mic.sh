98ffbcd824a623eaf87cd7344b8f7e7301c87592
Totti <tarekdshli@gmail.com>
2022-11-30 22:21:47 +0300

    added a script that creates a virtual audio cable (pipewire)

---
+++ bin/.scripts/Virtual-Mic.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Create a virtual sink that can be set as a monitor in OBS
+pactl load-module module-null-sink sink_name=VirtualSpeaker sink_properties=device.description=VirtualSpeaker
+
+# Link it with a virtual source that is visible in pulseaudio apps like Zoom
+pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=VirtualMic channel_map=front-left,front-right
+pw-link VirtualSpeaker:monitor_FL VirtualMic:input_FL
+pw-link VirtualSpeaker:monitor_FR VirtualMic:input_FR
