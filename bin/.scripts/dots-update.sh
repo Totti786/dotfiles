346b61ffbdc3c2969de7149a30325667b36392ab
Totti <tarekdshli@gmail.com>
2022-11-30 22:21:52 +0300

    Update dots-update.sh

--- bin/.scripts/dots-update.sh
+++ bin/.scripts/dots-update.sh
@@ -6,14 +6,26 @@ rofi_command="rofi -theme ~/.config/rofi/themes/window.rasi"
 # Variable passed to rofi
 options="Update System\nUpdate Configs\nExit"
 
+
+sysUpdate(){
+	alacritty --class 'alacritty-float,alacritty-float' -e sudo pacman -Syyu
+	if [[ "$1" -ne 0 ]]; then
+		echo "Update Failed" &&
+		wait 5
+	else
+		rm ~/.cache/pacman-updates
+	fi
+	}
+
+
 chosen="$(echo -e "$options" | $rofi_command -p 'Update Menu' -dmenu -selected-row 0)"
 case $chosen in
     Update\ System)
-		furminal --float -e yay
+		sysUpdate
         ;;
     Update\ Configs)
 		cd $dots
-		alacritty -e sh $HOME/dotfiles/install.sh --update
+		alacritty --class 'alacritty-float,alacritty-float' -e sh $HOME/dotfiles/install.sh --update
         ;;
     Exit)
 		exit
