5baf40d7d1b5d459e6f89ec67cf8f06fb2d5ac95
Totti <tarekdshli@gmail.com>
2022-12-07 16:58:07 +0300

    Update phoneWal.sh

--- bin/.scripts/phoneWal.sh
+++ bin/.scripts/phoneWal.sh
@@ -2,14 +2,14 @@
 
 ## Send a signal to phone to change wallpaper (requires automate to be set up)
 main(){
-if command -v kdeconnect-cli &> /dev/null; then 
+if command -v kdeconnect-cli &> /dev/null; then
 	deviceID="$(kdeconnect-cli -a --id-only)"
-	if [[ ! -z $deviceID ]]; then 
+	if [[ ! -z $deviceID ]]; then
 	kdeconnect-cli -d $deviceID --share \
 	$HOME/.config/wpg/templates/wallpaper.jpg && kdeconnect-cli -d $deviceID --ping
 	fi
 	exit
-else 
+else
 	echo "KDE Connect is not installed"
 fi
 }
