4800b3a0050180793e0f2b9a45e86731fa84a3e4
Totti <tarekdshli@gmail.com>
2022-12-09 19:04:36 +0300

    Update Virtual-Mic.sh

--- bin/.scripts/Virtual-Mic.sh
+++ bin/.scripts/Virtual-Mic.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-# Create a virtual sink that can be set as a monitor in OBS
-pactl load-module module-null-sink sink_name=VirtualSpeaker sink_properties=device.description=VirtualSpeaker
+if ! [ -f /tmp/vmic-lock ]; then create ; else echo "Virtaul Mic already working"; fi
 
-# Link it with a virtual source that is visible in pulseaudio apps like Zoom
-pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=VirtualMic channel_map=front-left,front-right
-pw-link VirtualSpeaker:monitor_FL VirtualMic:input_FL
-pw-link VirtualSpeaker:monitor_FR VirtualMic:input_FR
+create(){
+	touch /tmp/vmic-lock
+	
+	# Create a virtual sink that can be set as a monitor in OBS
+	pactl load-module module-null-sink sink_name=VirtualSpeaker sink_properties=device.description=VirtualSpeaker
+	
+	# Link it with a virtual source that is visible in pulseaudio apps like Zoom
+	pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=VirtualMic channel_map=front-left,front-right
+	pw-link VirtualSpeaker:monitor_FL VirtualMic:input_FL
+	pw-link VirtualSpeaker:monitor_FR VirtualMic:input_FR
+}
