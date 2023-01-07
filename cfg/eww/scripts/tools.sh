ca471e0450690f0d0b843af6581931d12caff5f5
Totti <tarekdshli@gmail.com>
2023-01-07 08:51:07 +0300

    Update Eww

--- cfg/eww/scripts/tools.sh
+++ cfg/eww/scripts/tools.sh
@@ -4,6 +4,8 @@ datecover(){
 	Wallpapers="$HOME/Pictures/Wallpapers"
 	cd $Wallpapers/Wallpapers.git/ &&
 	image=$(find -type f -not -path "./.git/*" | shuf -n 1)
-	ln -sf $Wallpapers/Wallpapers.git/${image:2} ~/.config/eww/actions/date-cover.jpg
+	convert $Wallpapers/Wallpapers.git/${image:2} -quality 30 ~/.cache/eww/cover.jpg
+	ln -sf ~/.cache/eww/cover.jpg ~/.config/eww/actions/date-cover.jpg
 	}
 datecover
+sed -i -e 's/:update .*/:update 1/g' ~/.config/eww/actions/actions.yuck
