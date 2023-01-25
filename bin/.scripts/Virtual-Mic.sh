488aec98c62dd2a1b570afb0f41ac4bb402ca5b5
Totti <tarekdshli@gmail.com>
2023-01-25 14:13:57 +0300

    Update Virtual-Mic.sh

--- bin/.scripts/Virtual-Mic.sh
+++ bin/.scripts/Virtual-Mic.sh
@@ -7,14 +7,11 @@ createMic(){
 	pactl load-module module-null-sink sink_name=VirtualSpeaker sink_properties=device.description=VirtualSpeaker
 	# Link it with a virtual source that is visible in pulseaudio apps like Zoom
 	pactl load-module module-null-sink media.class=Audio/Source/Virtual sink_name=VirtualMic channel_map=front-left,front-right
-}
-
-linkMic(){
-	touch /tmp/vmic-link-lock	
 	
 	pw-link VirtualSpeaker:monitor_FL VirtualMic:input_FL
 	pw-link VirtualSpeaker:monitor_FR VirtualMic:input_FR
 }
+
+
 if ! [ -f /tmp/vmic-lock ]; then createMic ; else echo "Virtaul Mic already working"; fi
-if ! [ -f /tmp/vmic-link-lock ]; then linkMic ; else echo "Virtaul Mic already linked"; fi
 
