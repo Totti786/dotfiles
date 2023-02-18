784dac38d7c4fe70c08870bedb95c50adeb5a8ac
Totti <tarekdshli@gmail.com>
2023-02-18 04:07:56 +0300

    Update controls.sh

--- cfg/eww/scripts/controls.sh
+++ cfg/eww/scripts/controls.sh
@@ -5,18 +5,9 @@ cache="$cache_dir/control-center.eww"
 active_players="$(playerctl -l | head -n 1)"
 
 [ ! -d $cache_dir ] && mkdir $cache_dir
-#sh ~/.config/eww/scripts/tools.sh datecover
 
 listen(){
 	while true ; do 
-		#id=$(wmctrl -l | awk '{print $1}' | xprop -root | grep _NET_ACTIVE_WINDOW | head -1 | awk '{print $5}' | sed 's/,//' | sed 's/^0x/0x0/')
-		#check=$(echo $(xwininfo -all -id $id | grep Fullscreen))
-		#if [[ $check == "Fullscreen" ]]; then
-			#xdo lower -N eww-controls
-		#else
-			#xdo raise -N eww-controls
-		#fi 
-	
 		if [[ ! -z "$(playerctl -l | head -n 1)" ]] &> /dev/null; then
 			eww update music-panel=true
 		else 
@@ -26,13 +17,22 @@ listen(){
 	done 
 }
 
-run () {
+run() {
 	eww open controls
 	polybar-msg action "#control.hook.0"
 	touch $cache
 	sleep 0.1 && listen &
 }
 
+close() {
+	[[ ! -z $active_players ]] && eww update music-panel=false
+	eww close-all
+	polybar-msg action "#control.hook.1"
+	eww update timer_reveal=false
+	rm $cache
+	killall -9 controls.sh
+}
+	
 if [[ ! `pidof eww` ]]; then
 	eww daemon &
 	sleep 0.5 &&
@@ -41,11 +41,6 @@ else
 	if [ ! -f $cache ]; then
 		run
 	else
-		[[ ! -z $active_players ]] && eww update music-panel=false
-		eww close-all
-		polybar-msg action "#control.hook.1"
-		eww update timer_reveal=false
-		rm $cache
-		killall -9 controls.sh
+		close
 	fi
 fi
