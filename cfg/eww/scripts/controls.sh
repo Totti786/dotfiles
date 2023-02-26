221be982aa0c7807b04704459dc69a97e94e53db
Totti <tarekdshli@gmail.com>
2023-02-27 01:34:37 +0300

    Update controls.sh

--- cfg/eww/scripts/controls.sh
+++ cfg/eww/scripts/controls.sh
@@ -8,54 +8,49 @@ config="$HOME/.config/eww"
 
 #if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then 
 	#_eww=eww-wayland
-	
 	### change the controls window location based on the display server
 	#sed -i 's/:x .*/:x "4.2%"/g' ~/.config/eww/controls/controls.yuck
 	#sed -i 's/:y .*/:y "31.25%"/g' ~/.config/eww/controls/controls.yuck
-	
 	#sed -i 's/:x .*/:x "4.2%"/g' ~/.config/eww/notifications/notification-panel.yuck 
 	#sed -i 's/:y .*/:y "43%"/g' ~/.config/eww/notifications/notification-panel.yuck 
-
 #else
 	#_eww=eww
-
 	### change the controls window location based on the display server
 	#sed -i 's/:x .*/:x "-0.5%"/g' ~/.config/eww/controls/controls.yuck
 	#sed -i 's/:y .*/:y "3.1%"/g' ~/.config/eww/controls/controls.yuck
-
 	#sed -i 's/:x .*/:x "-10px"/g' ~/.config/eww/notifications/notification-panel.yuck 
 	#sed -i 's/:y .*/:y "33px"/g' ~/.config/eww/notifications/notification-panel.yuck 
- 
-#fi
+ #fi
 
 open() {
-	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.0"
-	eww -c $config open controls
 	touch $cache
+	eww -c $config open controls &&
+	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.0"	
 }
 
 close() {
-	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.1"
-	eww -c $config close controls
 	rm $cache
+	eww -c $config close controls &&
+	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.1"
+	
 }
 
 #notification(){
 	#eww open --toggle notification-panel
 	#}
 	
-#main(){
-	#if [[ ! `pidof $_eww` ]]; then
-		#$_eww -c $config daemon &
-		#sleep 0.5 &&
-		#run
-	#else
+main(){
+	if [[ ! $(pidof eww) ]]; then
+		eww -c $config daemon &
+		sleep 0.5 &&
+		main
+	else
 		if [ ! -f $cache ]; then
 			open
 		else
 			close
 		fi
-	#fi
-#}
-
+	fi
+}
+main
 #if [[ ! "$1" ]];then main ;else notification ;fi
