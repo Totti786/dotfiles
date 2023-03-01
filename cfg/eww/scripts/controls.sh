39b48b1a82c7572e660191c5afcbd0dcadff2eed
Totti <tarekdshli@gmail.com>
2023-03-02 02:03:48 +0300

    eww update

--- cfg/eww/scripts/controls.sh
+++ cfg/eww/scripts/controls.sh
@@ -4,43 +4,26 @@ cache_dir="$HOME/.cache/eww"
 cache="$cache_dir/control-center.eww"
 config="$HOME/.config/eww"
 
-
+if [ -f "$HOME/.profile" ]; then source "$HOME/.profile" ; else  style="base" ;fi
 [ ! -d $cache_dir ] && mkdir $cache_dir
 
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then _eww=eww-wayland ;else _eww=eww ;fi
 
-#if [[ -f ~/.profile ]]; then source ~/.profile ;else style="base" ;fi
-
-#if [[ "$style" == "minimal" ]]; then
-	## change the controls window location based on the bar style
-	#sed -i 's/:x .*/:x "4.2%"/g' ~/.config/eww/controls/controls.yuck
-	#sed -i 's/:y .*/:y "31.25%"/g' ~/.config/eww/controls/controls.yuck
-	#sed -i 's/:x .*/:x "4.2%"/g' ~/.config/eww/notifications/notification-panel.yuck 
-	#sed -i 's/:y .*/:y "43%"/g' ~/.config/eww/notifications/notification-panel.yuck 
-#elif [[ "$style" == "base" ]]; then
-	## change the controls window location based on the bar style
-	#sed -i 's/:x .*/:x "-0.5%"/g' ~/.config/eww/controls/controls.yuck
-	#sed -i 's/:y .*/:y "3.1%"/g' ~/.config/eww/controls/controls.yuck
-	#sed -i 's/:x .*/:x "-10px"/g' ~/.config/eww/notifications/notification-panel.yuck 
-	#sed -i 's/:y .*/:y "33px"/g' ~/.config/eww/notifications/notification-panel.yuck 
-#fi
-
-
 open() {
 	touch $cache
-	eww -c $config open controls &&
+	eww -c $config open controls-"$XDG_SESSION_TYPE"-"$style" &&
 	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.0"	
 }
 
 close() {
 	rm $cache
 	[[ -z "$(playerctl -l | head -n 1)" ]] && eww -c $config update music-panel=false
-	eww -c $config close controls &&
+	eww -c $config close controls-"$XDG_SESSION_TYPE"-"$style" &&
 	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.1"
 }
 
 notification(){
-	eww -c $config open --toggle notification-panel
+	eww -c $config open --toggle notification-panel-"$XDG_SESSION_TYPE"-"$style"
 	}
 	
 main(){
