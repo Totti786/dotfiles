bf936677b0f9e87c7943082996143b5486cf02d1
Totti <tarekdshli@gmail.com>
2023-04-23 10:17:22 +0300

    Update phoneWal.sh

--- bin/.scripts/phoneWal.sh
+++ bin/.scripts/phoneWal.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+. "$HOME/.cache/wal/colors.sh"
+
 ## Send a signal to phone to change wallpaper (requires automate to be set up)
 main(){
 if command -v kdeconnect-cli &> /dev/null; then
 	deviceID="$(kdeconnect-cli -a --id-only)"
 	if [[ ! -z $deviceID ]]; then
-	kdeconnect-cli -d $deviceID --share \
-	$HOME/.config/wpg/templates/wallpaper.jpg && kdeconnect-cli -d $deviceID --ping
+		cp "$wallpaper" /tmp/wallpaper.jpg
+		kdeconnect-cli -d $deviceID --share /tmp/wallpaper.jpg && 
+		kdeconnect-cli -d $deviceID --ping
 	fi
 	exit
 else
