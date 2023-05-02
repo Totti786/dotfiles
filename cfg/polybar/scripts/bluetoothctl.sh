eac426ba43517acf3f2c86ea4cf34a5c9fe9a492
Totti <tarekdshli@gmail.com>
2023-05-02 18:52:17 +0300

    Update bluetoothctl.sh

--- cfg/polybar/scripts/bluetoothctl.sh
+++ cfg/polybar/scripts/bluetoothctl.sh
@@ -3,11 +3,11 @@
 get(){
 	btstatus="$(systemctl  status bluetooth.service | grep -i "Active:" | cut -d " " -f7)"
 	
-	if [[ $btstatus == "active" ]]; then
+	if [[ "$btstatus" == "active" ]]; then
 		if bluetoothctl show | grep -q "Powered: no"; then
 			echo " Bluetooth Off" 
 		elif bluetoothctl show | grep -q "Powered: yes"; then
-			if $(bluetoothctl devices Connected) &> /dev/null; then
+			if [[ -z $(bluetoothctl devices Connected) ]]; then
 				echo " Not Connected"
 			else 
 				echo " $(bluetoothctl devices Connected | head -n1 | cut -d " " -f3-7)"
@@ -23,8 +23,8 @@ connect(){
 	name=$(bluetoothctl devices Paired | head -n1 | cut -d " " -f3-7)
 
 	if bluetoothctl show | grep -q "Powered: yes"; then
-		if [[ $(bluetoothctl info) = "Missing device address argument" ]]; then
-			bluetoothctl connect $device && notify-send -r 98 -u low --appn-ame=Bluetooth "$name" "Connected"
+		if [[ "$(bluetoothctl info)" = "Missing device address argument" ]]; then
+			bluetoothctl connect "$device" && notify-send -r 98 -u low --app-name=Bluetooth "$name" "Connected"
 		else
 			bluetoothctl disconnect && notify-send -r 98 -u low --app-name=Bluetooth "$name" "Disconnected"
 		fi
