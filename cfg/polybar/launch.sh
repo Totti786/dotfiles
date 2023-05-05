feb9fea56f66d6819cae1b9ce0dd82c3144218f0
Totti <tarekdshli@gmail.com>
2023-05-05 07:35:41 +0300

    Update launch.sh

--- cfg/polybar/launch.sh
+++ cfg/polybar/launch.sh
@@ -9,9 +9,13 @@ network_interface="$(ip link | awk '/state UP/ {print $2}' | tr -d :)"
 battery_name=$(upower -i "$(upower -e | grep 'BAT')" | grep 'native-path' | cut -d':' -f2 | tr -d '[:blank:]')
 adapter_name=$(upower -i "$(upower -e | grep 'AC')" | grep 'native-path' | cut -d':' -f2 | tr -d '[:blank:]')
 current_desktop=$(wmctrl -m | head -n1 | cut -d " " -f2) 
+config_file="$dir/config.ini"
+minimal_config_file="$dir/minimal/config-minimal.ini"
+
+
 
 if [[ -f "$HOME/.zprofile" ]]; then 
-	source "$HOME"/.zprofile
+	source "$HOME/.zprofile"
 else
 	bar_style="base"
 	top_padding="23"
@@ -22,16 +26,17 @@ fi
 fix_modules() {
 	# check the graphics card and adjust the backlight module accordingly
 	if [[ -z "$graphics_card" ]]; then
-		sed -i -e 's/sep backlight/bna/g' 								   		"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
-		sed -i -e 's/sep brightness/bna/g' 								   		"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
+		sed -i -e 's/sep backlight/bna/g' 								   		"$config_file" "$minimal_config_file"
+		sed -i -e 's/sep brightness/bna/g' 								   		"$config_file" "$minimal_config_file"
 	elif [[ "$graphics_card" != *"intel_"* ]]; then
-		sed -i -e 's/backlight/brightness/g' 							   		"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
+		sed -i -e 's/backlight/brightness/g' 							   		"$config_file" "$minimal_config_file"
 	else
-		sed -i -e 's/bna/sep backlight/g' 								   		"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
+		sed -i -e 's/bna/sep backlight/g' 								   		"$config_file" "$minimal_config_file"
 	fi
 
 	# check if bspwm is the curren wm and changes the workspaces module 
-	if [[ "$current_desktop" == "bspwm" ]]; then
+	case "$current_desktop" in
+	 "bspwm")
 		bspc config -m focused top_padding "$top_padding"
 		bspc config -m focused bottom_padding "$bottom_padding"
 		sed -i -e 's/modules-center = \(i3\|workspaces\)/modules-center = bspwm/g'  \
@@ -41,41 +46,46 @@ fix_modules() {
 			-i -e 's/titlex\b/title/g'												\
 			-i -e 's/ssep/systray/g'												\
 			-i -e 's/tray-position = .*/tray-position = none/g'						\
-			-i -e 's/wm-restack = .*/wm-restack = bspwm/g' 							"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
-			
-	elif [[ "$current_desktop" == "herbstluftwm" ]]; then
+			-i -e 's/wm-restack = .*/wm-restack = bspwm/g' 							"$config_file" "$minimal_config_file"
+		;;
+	 "herbstluftwm")
 		sed -i -e 's/modules-center = \(i3\|bspwm\)/modules-center = workspaces/g' 	\
 			-i -e 's/override-redirect = .*/override-redirect = false/g' 			\
 			-i -e 's/titlex\b/title/g' 												\
 			-i -e 's/ssep/systray/g' 												\
 			-i -e 's/tray-position = .*/tray-position = none/g'						\
-			-i -e 's/wm-restack = .*/wm-restack = /g' 								"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
-			
-	elif [[ "$current_desktop" == "i3" ]]; then
+			-i -e 's/wm-restack = .*/wm-restack = /g' 								"$config_file" "$minimal_config_file"
+		;;	
+	 "i3")
 		i3-msg gaps top all set "$top_padding" &> /dev/null
 		i3-msg gaps bottom all set "$bottom_padding" &> /dev/null
 		sed -i -e 's/modules-center = \(bspwm\|workspaces\)/modules-center = i3/g' 	\
 			-i -e 's/override-redirect = .*/override-redirect = true/g' 			\
-			-i -e 's/scroll-up = .*/scroll-up = i3-msg workspace prev/g'			\
-			-i -e 's/scroll-down = .*/scroll-down = i3-msg workspace next/g' 		\
+			-i -e 's/scroll-up = .*/scroll-up = ~\/.config\/i3\/i3-workspaces prev/g' \
+			-i -e 's/scroll-down = .*/scroll-down = ~\/.config\/i3\/i3-workspaces next/g' \
 			-i -e 's/titlex\b/title/g' 												\
 			-i -e 's/systray/ssep/g'												\
 			-i -e 's/tray-position = .*/tray-position = right/g'					\
-			-i -e 's/wm-restack = .*/wm-restack = i3/g' 							"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
-			
-	elif [[ "$current_desktop" == "Openbox" ]]; then
+			-i -e 's/wm-restack = .*/wm-restack = i3/g' 							"$config_file" "$minimal_config_file"
+		;;	
+	 "Openbox")
 		openbox --reconfigure
 		sed -i -e 's/modules-center = \(i3\|bspwm\)/modules-center = workspaces/g'	\
 			-i -e 's/override-redirect = .*/override-redirect = false/g'			\
 			-i -e 's/title\b/titlex/g' 												\
 			-i -e 's/ssep/systray/g' 												\
 			-i -e 's/tray-position = .*/tray-position = none/g'						\
-			-i -e 's/wm-restack = .*/wm-restack = /g' 								"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
-	fi
+			-i -e 's/wm-restack = .*/wm-restack = /g' 								"$config_file" "$minimal_config_file"
+	 	;;
+	 	
+	 *)	 	 
+		echo "Unknown window manager: $current_desktop"
+		;;
+	esac
 	if [[ "$(pgrep picom)" ]]; then 
-		sed -i -e 's/pseudo-transparency = .*/pseudo-transparency = false/g'		"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
+		sed -i -e 's/pseudo-transparency = .*/pseudo-transparency = false/g'		"$config_file" "$minimal_config_file"
 	else
-		sed -i -e 's/pseudo-transparency = .*/pseudo-transparency = true/g'			"$dir"/config.ini "$dir"/minimal/config-minimal.ini 
+		sed -i -e 's/pseudo-transparency = .*/pseudo-transparency = true/g'			"$config_file" "$minimal_config_file"
 	fi
 
 }
