35f33e0196b6316387c1d7afab4ae17745ad84f4
Totti <tarekdshli@gmail.com>
2023-05-14 17:08:24 +0300

    Update dots-update.sh

--- bin/.scripts/dots-update.sh
+++ bin/.scripts/dots-update.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
-dots="$HOME/dotfiles"
-rofi_command="rofi -theme ~/.config/rofi/themes/layouts.rasi"
+rofi_command="rofi -theme $HOME/.config/rofi/themes/layouts.rasi"
+dots="$HOME/.dotfiles"
+
+if [ ! -d "$HOME/.dotfiles" ]; then 
+	cd "$dots"
+	furminal --float -e bash -c 'git clone --depth 1 https://github.com/totti786/dotfiles.git'
+fi
+
 
 # Variable passed to rofi
 options="Update System\nUpdate Configs\nUpdate Wallpapers\nExit"
@@ -9,19 +15,19 @@ options="Update System\nUpdate Configs\nUpdate Wallpapers\nExit"
 chosen="$(echo -e "$options" | $rofi_command -p 'Update Menu' -dmenu -selected-row 0)"
 case $chosen in
     "Update System")
-		alacritty --class 'alacritty-float,alacritty-float' --hold -e bash -c 'sudo pacman -Syu && dunstify --appname=pacman "Update Successful" && cat > ~/.cache/pacman-updates'
+		furminal --float -e bash -c 'sudo pacman -Syu && dunstify --appname=pacman "Update Successful" && cat > ~/.cache/pacman-updates'
         ;;
     "Update Configs")
    		cd "$dots"
-		alacritty --class 'alacritty-float,alacritty-float' -e sh $dots/install.sh --update &&
+		furminal --float -e sh "$dots"/install.sh --update &&
 		## remove already existing json file for background color scheme
 		wpg -R "$dots"/deps/background.jpg 
 		## change wallpaper and update color scheme 
-		sh "$dots"/bin/.local/bin/wpgtk wall "$dots"/deps/background.jpg 
+		"$dots"/bin/.local/bin/wpgtk wall "$dots"/deps/background.jpg 
         ;;
     "Update Wallpapers")
-		cd $dots
-		alacritty --class 'alacritty-float,alacritty-float' -e sh $HOME/dotfiles/install.sh --wallpapers
+		cd "$dots"
+		furminal --float -e sh "$dots"/install.sh --wallpapers
         ;;
     "Exit")
 		exit
