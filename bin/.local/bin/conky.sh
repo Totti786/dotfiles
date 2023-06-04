a7098021c4c503f0b16618c6927def962f83273e
Totti786 <tarekdshli@gmail.com>
2023-06-04 04:00:36 +0300

    Update conky.sh

--- bin/.local/bin/conky.sh
+++ bin/.local/bin/conky.sh
@@ -2,11 +2,10 @@
 
 source "$HOME/.cache/wal/colors.sh"
 res="$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d 'x' -f2)"
-#wm="$(wmctrl -m | head -n1 | cut -d " " -f2)"
 conkyConfig="$HOME/.config/conky/conky-$XDG_CURRENT_DESKTOP.conf"
 
 [[ -f "$HOME/.zprofile" ]] && source "$HOME/.zprofile"
-[[ "$bar_style" == "bionic" ]] && alpha="255" || alpha="140"
+[[ "$bar_style" == "bionic" ]] && alpha="255" || alpha="190"
 
 fixconfig(){
 	if [[ "$res" == "1080" ]]; then
@@ -14,15 +13,17 @@ fixconfig(){
 			-i -e "s/minimum_width = .*,/minimum_width = 330,/g" \
 			-i -e "s/font  = 'JetBrains Mono:Bold:size=.*',/font  = 'JetBrains Mono:Bold:size=10',/g" \
 			-i -e "s/font1 = 'JetBrains Mono:Bold:size=.*',/font1 = 'JetBrains Mono:Bold:size=15',/g" \
-			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=25',/g" "$conkyConfig"
-	elif [[ "$res" == "768" ]]; then 
+			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=25',/g" \
+		"$conkyConfig"
+	elif [[ "$res" == "768" ]]; then
 		sed -i -e "s/gap_y = .*,/gap_y = 70,/g" \
 			-i -e "s/minimum_width = .*,/minimum_width = 280,/g" \
 			-i -e "s/font  = 'JetBrains Mono:Bold:size=.*',/font  = 'JetBrains Mono:Bold:size=8',/g" \
 			-i -e "s/font1 = 'JetBrains Mono:Bold:size=.*',/font1 = 'JetBrains Mono:Bold:size=11',/g" \
-			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=20',/g" "$conkyConfig"
+			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=20',/g" \
+		"$conkyConfig"
 	fi
-	sed -i -e "s/own_window_colour = '#.*',/own_window_colour = '$(pastel mix -f 0.6 $color8 $color0 | pastel format hex)',/g" \
+	sed -i -e "s/own_window_colour = '#.*',/own_window_colour = '$(pastel mix -f 0.4 $color8 $color0 | pastel format hex)',/g" \
 		-i -e "s/color0 = '#.*',/color0 = '${color9}',/g" \
 		-i -e "s/color1 = '#.*',/color1 = '${foreground}',/g" \
 		-i -e "s/color2 = '#.*',/color2 = '${color12}',/g" \
@@ -35,7 +36,7 @@ fixconfig(){
 toggle(){
 	if [ -z "$(pgrep conky)" ]; then 
 		fixconfig
-		conky -c $conkyConfig &> /dev/null
+		conky -c "$conkyConfig" &> /dev/null
 		[[ "$XDG_CURRENT_DESKTOP" == "bspwm" ]] && xdo lower -N Conky
 	else
 		pkill conky
@@ -46,9 +47,9 @@ restart(){
 	if [ ! -z "$(pgrep conky)" ]; then 
 		pkill conky &&
 		fixconfig
-		conky -c $conkyConfig &> /dev/null
+		conky -c "$conkyConfig" &> /dev/null
 		[[ "$XDG_CURRENT_DESKTOP" == "bspwm" ]] && xdo lower -N Conky
 	fi
 	}
 		
-if [[ $1 == "restart" ]]; then restart ;else toggle ;fi
+if [[ "$1" == "restart" ]]; then restart ;else toggle ;fi
