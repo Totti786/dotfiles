1a039423acdf24c7a3f7f0c3d0142426f1adef74
Totti786 <tarekdshli@gmail.com>
2023-06-12 05:15:43 +0300

    Update playerctl.sh

--- cfg/polybar/scripts/playerctl.sh
+++ cfg/polybar/scripts/playerctl.sh
@@ -5,105 +5,62 @@
 [[ -z "$bar_style" ]] && bar_style="base"
 [[ "$bar_style" == "base" ]] && PARENT_BAR="bottom" || PARENT_BAR="main"
 
-PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | cut -d" " -f1)
+PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | awk '{print $1}')
 
 # Sends $2 as message to all polybar PIDs that are part of $1
 update_hooks() {
-    while IFS= read -r id
-    do
+    while IFS= read -r id ;do
         polybar-msg -p "$id" hook playerctl-playpause "$2" 1>/dev/null 2>&1
-    done < <(echo "$1")
+    done <<< "$1"
 }
 
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
 # See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
 Format="{{ title }}: {{ artist }}"
-Duration="{{duration(position) }} "/" {{ duration(mpris:length)}}"
 
-player(){
-	Current="$(playerctl metadata --format "$Format")"
+player() {
+	Current="$(playerctl metadata --format "$Format" 2>/dev/null)"
 	PlayerName="$(playerctl -l | head -n1 | cut -f1 -d ".")"
 	case $PlayerName in
-	  spotify) echo "$Current "
-	  ;;
-	  firefox) echo "$Current "
-	  ;;
-	  kdeconnect) echo "$Current "
-	  ;;  
-	  vlc) echo "$Current 嗢"
-	  ;;  
-	  mpv) echo "$Current "
-	  ;;  
-	  rhythmbox) echo "$Current 蓼"
-	  ;;
-	  *) echo "$Current"
+	  spotify) echo "$Current  " ;;
+	  firefox) echo "$Current  " ;;
+	  kdeconnect) echo "$Current  " ;;  
+	  vlc) echo "$Current 嗢 " ;;  
+	  mpv) echo "$Current  " ;;  
+	  rhythmbox) echo "$Current 蓼 " ;;
+	  *) echo "$Current" ;;
 	esac
 }
-duration(){
-	CurrentD="$(playerctl -s metadata --format "$Duration")"
-	PlayerName="$(playerctl -l | head -n1 | cut -f1 -d ".")"
-	case $PlayerName in
-	  spotify) echo " $CurrentD "
-	  ;;
-	  firefox) echo " "
-	  ;;
-	  kdeconnect) echo " $CurrentD "
-	  ;;  
-	  vlc) echo " $CurrentD 嗢"
-	  ;;  
-	  mpv) echo " $CurrentD "
-	  ;;  
-	  rhythmbox) echo " $CurrentD 蓼"
-	  ;;
-	  *) echo ""
-	esac
-	}
 
 PLAYERCTL_STATUS=$(playerctl status 2>/dev/null)
-EXIT_CODE=$?
 
-if [ $EXIT_CODE -eq 0 ]; then
+if [ $? -eq 0 ]; then
     STATUS=$PLAYERCTL_STATUS
 else
     STATUS="No player is running"
 fi
 
-if [ "$1" == "--status" ]; then
-    echo "$STATUS"
-elif [ "$1" == "--name" ]; then
-    if [ "$STATUS" = "Stopped" ]; then
-        echo "No music is playing"
-    elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
-        player
-    elif [ "$STATUS" = "No player is running"  ]; then
-        echo "Doesn't look like anything to me"
-    else
-        update_hooks "$PARENT_BAR_PID" 1
-		player
-    fi
-elif [ "$1" == "--name-minimal" ]; then
-    if [ "$STATUS" = "Stopped" ]; then
-        echo "No music is playing"
-    elif [ "$STATUS" = "Paused"  ]; then
-        update_hooks "$PARENT_BAR_PID" 2
-        player
-    elif [ "$STATUS" = "No player is running"  ]; then
-        echo ""
-        update_hooks "$PARENT_BAR_PID" 3
-    else
-        update_hooks "$PARENT_BAR_PID" 1
-		player
-    fi
-elif [ "$1" == "--duration" ]; then
-	 if [ "$STATUS" = "Stopped" ]; then
-        echo ""
-    elif [ "$STATUS" = "Paused"  ]; then
-        duration
-    elif [ "$STATUS" = "No player is running"  ]; then
-        echo ""
-    else
-		duration
-	fi
-fi
+case "$1" in
+    "--status")
+        echo "$STATUS"
+        ;;
+    "--name" | "--name-minimal")
+        if [ "$STATUS" = "Stopped" ]; then
+            echo "No music is playing"
+        elif [ "$STATUS" = "Paused"  ]; then
+            update_hooks "$PARENT_BAR_PID" 2
+            player
+        elif [ "$STATUS" = "No player is running" ]; then
+            if [ "$1" = "--name-minimal" ]; then
+       	        echo ""
+                update_hooks "$PARENT_BAR_PID" 3
+            else
+                echo "Doesn't look like anything to me"
+            fi
+        else
+            update_hooks "$PARENT_BAR_PID" 1
+            player
+        fi
+        ;;
+esac
