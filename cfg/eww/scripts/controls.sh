03836a32e4e0dae13e7c8fd678150b95b5ac4fbf
Totti786 <tarekdshli@gmail.com>
2023-06-12 05:17:33 +0300

    Update controls.sh

--- cfg/eww/scripts/controls.sh
+++ cfg/eww/scripts/controls.sh
@@ -1,18 +1,23 @@
-##!/bin/bash
+#!/usr/bin/env bash
 
 cache_dir="$HOME/.cache/eww"
 cache="$cache_dir/control-center.eww"
 config="$HOME/.config/eww"
 
-if [ -f "$HOME/.zprofile" ]; then source "$HOME/.zprofile" ; else  bar_style="base" ;fi
-[ ! -d $cache_dir ] && mkdir $cache_dir
+[[ -f "$HOME/.zprofile" ]] && source "$HOME/.zprofile"
+[[ -z "$bar_style" ]] && bar_style="base" top_padding="23" bottom_padding="23"
+[[ "$top_padding" == "0" ]] && anchor="bottom right" padding="-$(( $bottom_padding + 10))" || anchor="top right" padding="$(( $top_padding + 10))"
 
+[ ! -d $cache_dir ] && mkdir $cache_dir
 
 open() {
 	touch $cache
-	[[ "$XDG_SESSION_TYPE" == "x11" ]] && eww -c $config open --toggle background-closer
-	eww -c $config open controls-"$XDG_SESSION_TYPE"-"$bar_style" &&
+	#sed -i -e "s/:y .*/:y \"${padding}\"/" \
+		    -e "s/:anchor .*/:anchor \"$anchor\"/"  \
+		"$config/controls/controls.yuck"
+	eww -c $config open controls-"$XDG_SESSION_TYPE" &&
 	[[ $(pgrep polybar) ]] && polybar-msg action "#control.hook.0"
+#		[[ "$XDG_SESSION_TYPE" == "x11" ]] && eww -c $config open --toggle background-closer
 	}
 
 close() {
@@ -23,12 +28,18 @@ close() {
 }
 
 notification(){
-	eww -c $config open --toggle notification-panel-"$XDG_SESSION_TYPE"-"$bar_style"
+	sed -i -e "s/:y .*/:y \"${padding}\"/" \
+		   -e "s/:anchor .*/:anchor \"$anchor\"/" \
+	"$config/notifications/notification-panel.yuck"
+	eww -c $config open --toggle notification-panel-"$XDG_SESSION_TYPE"
 	}
 
 main(){
 	if [[ ! $(pidof eww) ]] && [[ "$XDG_SESSION_TYPE" == "x11" ]] ; then
 		[[ $(pidof eww-wayland) ]] && pkill eww 
+		sed -i -e "s/:y .*/:y \"${padding}\"/" \
+		    -e "s/:anchor .*/:anchor \"$anchor\"/"  \
+		"$config/controls/controls.yuck" "$config/notifications/notification-panel.yuck"
 		eww -c $config daemon &
 		sleep 0.5 &&
 		main
