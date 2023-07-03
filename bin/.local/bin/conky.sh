9e4ddb1a95e35ee6868f4e08a32dfd78519efcf7
Totti786 <tarekdshli@gmail.com>
2023-07-03 20:08:49 +0300

    Update conky.sh

--- bin/.local/bin/conky.sh
+++ bin/.local/bin/conky.sh
@@ -1,55 +1,54 @@
 #!/usr/bin/env bash
 
-source "$HOME/.cache/wal/colors.sh"
-res="$(xdpyinfo | awk '/dimensions/{print $2}' | cut -d 'x' -f2)"
-conkyConfig="$HOME/.config/conky/conky-$XDG_CURRENT_DESKTOP.conf"
+config="$HOME/.config/conky/conky-$XDG_CURRENT_DESKTOP.conf"
 
 [[ -f "$HOME/.zprofile" ]] && source "$HOME/.zprofile"
 [[ "$bar_style" == "bionic" ]] && alpha="255" || alpha="190"
 
-fixconfig(){
-	if [[ "$res" == "1080" ]]; then
-		sed -i -e "s/gap_y = .*,/gap_y = 162,/g" \
-			-i -e "s/minimum_width = .*,/minimum_width = 330,/g" \
-			-i -e "s/font  = 'JetBrains Mono:Bold:size=.*',/font  = 'JetBrains Mono:Bold:size=10',/g" \
-			-i -e "s/font1 = 'JetBrains Mono:Bold:size=.*',/font1 = 'JetBrains Mono:Bold:size=15',/g" \
-			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=25',/g" \
-		"$conkyConfig"
-	elif [[ "$res" == "768" ]]; then
-		sed -i -e "s/gap_y = .*,/gap_y = 70,/g" \
-			-i -e "s/minimum_width = .*,/minimum_width = 280,/g" \
-			-i -e "s/font  = 'JetBrains Mono:Bold:size=.*',/font  = 'JetBrains Mono:Bold:size=8',/g" \
-			-i -e "s/font1 = 'JetBrains Mono:Bold:size=.*',/font1 = 'JetBrains Mono:Bold:size=11',/g" \
-			-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=20',/g" \
-		"$conkyConfig"
-	fi
-	sed -i -e "s/own_window_colour = '#.*',/own_window_colour = '$(pastel mix -f 0.4 $color8 $color0 | pastel format hex)',/g" \
-		-i -e "s/color0 = '#.*',/color0 = '${color10}',/g" \
-		-i -e "s/color1 = '#.*',/color1 = '${foreground}',/g" \
-		-i -e "s/color2 = '#.*',/color2 = '${color13}',/g" \
-		-i -e "s/color3 = '#.*',/color3 = '${color14}',/g" \
-		-i -e "s/own_window_argb_value = .*/own_window_argb_value = $alpha,/g" \
-	"$conkyConfig"	
-	}
+if [[ -f "$HOME/.cache/wal/colors.sh" ]]; then
+	source "$HOME/.cache/wal/colors.sh"
+else
+	FG="#FFFFFF"
+	red="#7ba5dd"
+	blue="#ee6a70"
+	yellow="#7ba5dd"
+	green="#7ba5dd"
+	magenta="#ee6a70"
+fi
+
+
+#sed -i -e "s/gap_y = .*,/gap_y = 162,/g" \
+	#-i -e "s/minimum_width = .*,/minimum_width = 330,/g" \
+	#-i -e "s/font  = 'JetBrains Mono:Bold:size=.*',/font  = 'JetBrains Mono:Bold:size=10',/g" \
+	#-i -e "s/font1 = 'JetBrains Mono:Bold:size=.*',/font1 = 'JetBrains Mono:Bold:size=15',/g" \
+	#-i -e "s/font2 = 'JetBrains Mono:bold:size=.*',/font2 = 'JetBrains Mono:bold:size=25',/g" \
+#"$conkyConfig"
+
+sed -i -e "s/own_window_colour = '#.*',/own_window_colour = '$(pastel mix -f 0.4 $color8 $color0 | pastel format hex)',/g" \
+	-i -e "s/color0 = '#.*',/color0 = '${color10}',/g" \
+	-i -e "s/color1 = '#.*',/color1 = '${foreground}',/g" \
+	-i -e "s/color2 = '#.*',/color2 = '${color13}',/g" \
+	-i -e "s/color3 = '#.*',/color3 = '${color14}',/g" \
+	-i -e "s/own_window_argb_value = .*/own_window_argb_value = $alpha,/g" \
+"$config"	
+
 
 
 toggle(){
-	if [ -z "$(pgrep conky)" ]; then 
-		fixconfig
-		conky -c "$conkyConfig" &> /dev/null
+	if [[ -z "$(pgrep conky)" ]]; then 
+		conky -qc "$config"
 		[[ "$XDG_CURRENT_DESKTOP" == "bspwm" ]] && xdo lower -N Conky
 	else
-		pkill conky
+		pkill -9 conky
 	fi
-	}
+}
 
 restart(){
-	if [ -n "$(pgrep conky)" ]; then 
-		pkill conky &&
-		fixconfig
-		conky -c "$conkyConfig" &> /dev/null
+	if [[ -n "$(pgrep conky)" ]]; then 
+		toggle
+		conky -qc "$config"
 		[[ "$XDG_CURRENT_DESKTOP" == "bspwm" ]] && xdo lower -N Conky
 	fi
-	}
+}
 		
 if [[ "$1" == "restart" ]]; then restart ;else toggle ;fi
