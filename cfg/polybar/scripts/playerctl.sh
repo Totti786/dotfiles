d0e4a52a3f25f7c37e35dca7198525c73f615c3d
Totti786 <tarekdshli@gmail.com>
2023-10-13 22:25:19 +0300

    Update playerctl.sh

--- cfg/polybar/scripts/playerctl.sh
+++ cfg/polybar/scripts/playerctl.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # The name of polybar bar which houses the main spotify module and the control modules.
 [[ -f "$HOME/.zprofile" ]] && source "$HOME/.zprofile"
 [[ -z "$bar_style" ]] && bar_style="base"
-[[ "$bar_style" == "base" ]] && PARENT_BAR="bottom" || PARENT_BAR="main"
+[[ "$bar_style" == "base" ]] && parent_bar="bottom" || parent_bar="main"
 
-PARENT_BAR_PID=$(pgrep -a "polybar" | grep "$PARENT_BAR" | awk '{print $1}')
+parent_bar_id=$(pgrep -a "polybar" | grep "$parent_bar" | awk '{print $1}')
 
 # Sends $2 as message to all polybar PIDs that are part of $1
 update_hooks() {
@@ -17,49 +17,55 @@ update_hooks() {
 # Format of the information displayed
 # Eg. {{ artist }} - {{ album }} - {{ title }}
 # See more attributes here: https://github.com/altdesktop/playerctl/#printing-properties-and-metadata
-Format="{{ title }}: {{ artist }}"
+if [[ -z $(playerctl metadata --format "{{ artist }}") ]] 2>/dev/null; then
+	format="{{ title }}"
+else
+	format="{{ title }}: {{ artist }}"
+fi
 
 player() {
-	Current="$(playerctl metadata --format "$Format" 2>/dev/null)"
-	PlayerName="$(playerctl -l | head -n1 | cut -f1 -d ".")"
-	case $PlayerName in
-	  spotify) echo "$Current  " ;;
-	  firefox) echo "$Current  " ;;
-	  kdeconnect) echo "$Current  " ;;  
-	  vlc) echo "$Current 嗢 " ;;  
-	  mpv) echo "$Current  " ;;  
-	  rhythmbox) echo "$Current 蓼 " ;;
-	  *) echo "$Current" ;;
-	esac
+	current="$(playerctl metadata --format "$format" 2>/dev/null)"
+	player_name="$(playerctl -l | head -n1 | cut -f1 -d ".")"
+	declare -A player_icons=(
+		["spotify"]=""
+		["firefox"]=""
+		["kdeconnect"]=""
+		["vlc"]="嗢"
+		["mpv"]=""
+		["rhythmbox"]="蓼"
+	)
+	
+	icon="${player_icons[$player_name]}"
+	echo "$current ${icon:-}"
 }
 
-PLAYERCTL_STATUS=$(playerctl status 2>/dev/null)
+playerctl_status=$(playerctl status 2>/dev/null)
 
 if [ $? -eq 0 ]; then
-    STATUS=$PLAYERCTL_STATUS
+    status="$playerctl_status"
 else
-    STATUS="No player is running"
+    status="No player is running"
 fi
 
 case "$1" in
     "--status")
-        echo "$STATUS"
+        echo "$status"
         ;;
     "--name" | "--name-minimal")
-        if [ "$STATUS" = "Stopped" ]; then
+        if [ "$status" = "Stopped" ]; then
             echo "No music is playing"
-        elif [ "$STATUS" = "Paused"  ]; then
-            update_hooks "$PARENT_BAR_PID" 2
+        elif [ "$status" = "Paused"  ]; then
+            update_hooks "$parent_bar_id" 2
             player
-        elif [ "$STATUS" = "No player is running" ]; then
+        elif [ "$status" = "No player is running" ]; then
             if [ "$1" = "--name-minimal" ]; then
        	        echo ""
-                update_hooks "$PARENT_BAR_PID" 3
+                update_hooks "$parent_bar_id" 3
             else
                 echo "Doesn't look like anything to me"
             fi
         else
-            update_hooks "$PARENT_BAR_PID" 1
+            update_hooks "$parent_bar_id" 1
             player
         fi
         ;;
