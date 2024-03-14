048dde74fcd0432de1296eda9a201a329d51ccd1
Totti <63831322+Totti786@users.noreply.github.com>
2024-03-14 21:36:52 +0300

    Update auto-sc.sh

--- bin/.scripts/auto-sc.sh
+++ bin/.scripts/auto-sc.sh
@@ -1,25 +1,39 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-DIR="$HOME/Pictures/Screenshots/Temp"
-[[ ! -d "$DIR" ]] && mkdir -p "$DIR"
+current_day=$(date +'%m-%d-%Y-%A')
+temp_dir_base="$HOME/Pictures/Screenshots/Temp"
+temp_dir="$temp_dir_base/$current_day"
 
-sc(){
-	cd "$DIR"
-	while true; do
-		time=$(date +%Y-%m-%d-%H%M%S)
-		if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
-			scrot -q 70 "$DIR/$time.png"
-		else
-			grim "$DIR/$time.png"
-		fi
-		sleep 45
-	done
-}
-
-if command -v scrot &> /dev/null || command -v grim &> /dev/null ; then
-	if [[ "$(pgrep auto-sc.sh | wc -l)" > 2 ]]; then
-		echo "Script already running"
-	else
-		sc
-	fi
+if [[ ! -d "$temp_dir" ]]; then
+	mkdir -p "$temp_dir"
 fi
+
+if ! command -v scrot &> /dev/null || ! command -v grim &> /dev/null ; then
+	echo "scrot or grim are not installed!"
+	exit 1
+fi
+
+if [[ "$(pgrep -f auto-sc.sh | wc -l)" > 2 ]]; then
+	echo "Script already running"
+	exit 0
+fi
+
+while true; do
+    new_day=$(date +'%m-%d-%Y-%A')
+    if [[ "$new_day" != "$current_day" ]]; then
+        current_day="$new_day"
+        temp_dir="$temp_dir_base/$current_day"  # Set to the parent directory
+        mkdir -p "$temp_dir"
+    fi
+
+    time=$(date +%Y-%m-%d-%H%M%S)
+
+    if [[ "$XDG_SESSION_TYPE" == "x11" ]]; then
+        scrot -q 70 "$temp_dir/$time.png"
+    else
+        grim "$temp_dir/$time.png"
+    fi
+
+    sleep 45
+done
+
