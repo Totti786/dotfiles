56128d619d26cd8b5aea9bf21bac5c6a5113bb9f
totti786 <63831322+Totti786@users.noreply.github.com>
2024-05-04 05:07:09 +0300

    Update dots-update.sh

--- cfg/ags/scripts/quickscripts/dots-update.sh
+++ cfg/ags/scripts/quickscripts/dots-update.sh
@@ -6,15 +6,20 @@ dots="$HOME/.dotfiles"
 if [[ ! -d "$dots" ]]; then 
 	mkdir "$dots"
 	git clone --depth 1 https://github.com/totti786/dotfiles.git "$dots"
-
+	
+	tput -x clear
+	
 	cd "$dots"
 	if [[ "$(git rev-parse --is-inside-work-tree)" != "true" ]]; then 
-		rm -r "$dots" && notify-send -r 34 'Cloning Failed' 
+		rm -rf "$dots" && notify-send -r 34 'Cloning Failed' 
+		echo "Cloning Failed, Try again"
 		exit 1
+	else
+		echo "Cloning is complete!"
+		echo "You can now close this window"
 	fi
 else
-	#cd "$dots"
-	#"$dots"/install.sh --update &&
+	"$dots"/install.sh --update &&
 	## remove already existing json file for background color scheme
 	rm "$HOME/.config/wpg/schemes/_home_$(whoami)__dotfiles_deps_background_jpg_dark_wal__1.1.0.json"
 	## change wallpaper and update color scheme
