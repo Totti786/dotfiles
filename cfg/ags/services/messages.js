1763be04dca9b69b0afa8cdfc564feb4d0ee1263
totti786 <63831322+Totti786@users.noreply.github.com>
2024-06-01 17:36:17 +0300

    ags update

--- cfg/ags/services/messages.js
+++ cfg/ags/services/messages.js
@@ -14,26 +14,11 @@ const APP_NAME = "ags";
 const FIRST_RUN_NOTIF_TITLE = "Welcome!";
 const FIRST_RUN_NOTIF_BODY = `First run? 👀 <span foreground="#FF0202" font_weight="bold">SUPER+Shift+W</span> to pick a wallpaper (or styles will break!)\nFor a list of keybinds, hit <span foreground="#c06af1" font_weight="bold">Super + /</span>.`;
 
-export async function firstRunWelcome() {
-    GLib.mkdir_with_parents(`${GLib.get_user_state_dir()}/ags/user`);
-    if (!fileExists(FIRST_RUN_PATH)) {
-        Utils.writeFile(FIRST_RUN_FILE_CONTENT, FIRST_RUN_PATH)
-            .then(() => {
-                // Note that we add a little delay to make sure the cool circular progress works
-                Utils.execAsync(['hyprctl', 'keyword', 'bind', "Super,Slash,exec,ags -t cheatsheet"]).catch(print);
-                Utils.execAsync(['bash', '-c',
-                    `sleep 0.5; notify-send "Millis since epoch" "$(date +%s%N | cut -b1-13)"; sleep 0.5; notify-send '${FIRST_RUN_NOTIF_TITLE}' '${FIRST_RUN_NOTIF_BODY}' -a '${APP_NAME}' &`
-                ]).catch(print)
-            })
-            .catch(print);
-    }
-}
-
 var batteryWarned = false;
 async function batteryMessage() {
     const perc = Battery.percent;
     const charging = Battery.charging;
-    if(charging) {
+    if (charging) {
         batteryWarned = false;
         return;
     }
@@ -46,16 +31,31 @@ async function batteryMessage() {
             break;
         }
     }
-    if(perc <= userOptions.battery.suspendThreshold) {
+    if (perc <= userOptions.battery.suspendThreshold) {
         Utils.execAsync(['bash', '-c',
-                `notify-send "Suspending system" "Critical battery level (${perc}% remaining)" -u critical -a '${APP_NAME}' -t 69420 &`
-            ]).catch(print);
+            `notify-send "Suspending system" "Critical battery level (${perc}% remaining)" -u critical -a '${APP_NAME}' -t 69420 &`
+        ]).catch(print);
         Utils.execAsync('systemctl suspend').catch(print);
     }
 }
 
-// Run them
-firstRunWelcome().catch(print);
-Utils.timeout(1, () => {
-    Battery.connect('changed', () => batteryMessage().catch(print));
-})
+export async function startBatteryWarningService() {
+    Utils.timeout(1, () => {
+        Battery.connect('changed', () => batteryMessage().catch(print));
+    })
+}
+
+export async function firstRunWelcome() {
+    GLib.mkdir_with_parents(`${GLib.get_user_state_dir()}/ags/user`, 755);
+    if (!fileExists(FIRST_RUN_PATH)) {
+        Utils.writeFile(FIRST_RUN_FILE_CONTENT, FIRST_RUN_PATH)
+            .then(() => {
+                // Note that we add a little delay to make sure the cool circular progress works
+                Utils.execAsync(['hyprctl', 'keyword', 'bind', "Super,Slash,exec,ags -t cheatsheet"]).catch(print);
+                Utils.execAsync(['bash', '-c',
+                    `sleep 0.5; notify-send "Millis since epoch" "$(date +%s%N | cut -b1-13)"; sleep 0.5; notify-send '${FIRST_RUN_NOTIF_TITLE}' '${FIRST_RUN_NOTIF_BODY}' -a '${APP_NAME}' &`
+                ]).catch(print)
+            })
+            .catch(print);
+    }
+}
