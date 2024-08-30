d71c1bf8e2abaf9f18fe40873e5bd12ddaa7f246
totti786 <63831322+Totti786@users.noreply.github.com>
2024-08-30 14:18:02 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ declare -a minimal=(
 	bluez-utils brightnessctl bspwm clight conky copyq dmenu drawing dunst envycontrol \
 	fd feh file-roller firefox flameshot fluent-cursor-theme-git font-manager fzf gammastep \
 	geany gnome-bluetooth-3.0 gnome-calculator gnome-disk-utility gnome-epub-thumbnailer \
-	gpu-screen-recorder-git jq gpick grep python-gtts htop i3lock-color i3-wm imagemagick jgmenu \
-	kdeconnect libplasma linux-wifi-hotspot man moreutils mpv mpv-mpris mugshot ncdu \
+	gpu-screen-recorder-git jq gpick grep python-gtts html-xml-utils htop i3lock-color i3-wm \
+	imagemagick jgmenu kdeconnect libplasma linux-wifi-hotspot man moreutils mpv mpv-mpris mugshot ncdu \
 	network-manager-applet networkmanager-openvpn noto-fonts noto-fonts-cjk noto-fonts-emoji nsxiv \
 	nvtop obconf openbox openssh openvpn papirus-icon-theme pastel pavucontrol qt5ct rhythmbox rofi-wayland \
 	rtorrent scrot stalonetray snapshot sxhkd termdown thunar thunar-archive-plugin paper perl plank \
